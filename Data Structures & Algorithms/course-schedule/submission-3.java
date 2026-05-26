5d4c2ee906ee1abf1b511824612dd0b0c5043035
Mai Đại Lâm <maidailam2001@gmail.com>
2026-05-26 21:16:00 +0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+    // Time Complexity: O(V + E)
+    // Space Complexity: O(V + E)
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 1) return true;
+        if (prerequisites.length == 0) return true;
+
+        int[] inDegree = new int[numCourses];
+        List<Integer>[] adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        // build inDegre and adjList
+        for (int[] pre : prerequisites) {
+            inDegree[pre[0]]++;
+            adjList[pre[1]].add(pre[0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            for (int nextCourse : adjList[course]) {
+                inDegree[nextCourse]--;
+                if (inDegree[nextCourse] == 0) {
+                    q.add(nextCourse);
+                }
+            }
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }    
+}
