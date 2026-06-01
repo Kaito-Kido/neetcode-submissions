c0c2a7dc5a3299b273e4fbe716bb1309644987f8
Mai Đại Lâm <maidailam2001@gmail.com>
2026-06-01 11:21:47 +0700

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    // Time Complexity: O(m x n)
+    // Space ComplextiY: O(m x n) - haven't optimize, could be optimize to O(Min(m, n));
+    public int longestCommonSubsequence(String text1, String text2) {
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1]; // space for base case empty string;
+
+        for (int i = 1; i <= l1; i++) {
+            char currentChar1 = text1.charAt(i - 1);
+            for (int j = 1; j <= l2; j++) {
+                char currentChar2 = text2.charAt(j - 1);
+                if (currentChar1 == currentChar2) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[l1][l2];
+    }
+}
