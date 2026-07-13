6570f3be9275e23548fbea954665697746549aeb
Mai Đại Lâm <maidailam2001@gmail.com>
2026-07-13 22:52:15 +0700

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        contains = defaultdict(int)
+    
+        for c in t:
+            contains[c] += 1
+        
+        left = 0
+        right = 0
+        counter = 0
+        min_left = 0
+        minsublen = len(s)
+        containsAll = False
+        
+        while left < len(s) and right < len(s):
+            c = s[right]
+            if c in contains:
+                contains[c] -= 1
+                if contains[c] == 0:
+                    counter += 1
+            
+            while counter == len(contains) and left < len(s):
+                containsAll = True
+                if right - left + 1 < minsublen:
+                    minsublen = right - left + 1
+                    min_left = left
+                if s[left] in contains:
+                    contains[s[left]] += 1
+                    if contains[s[left]] > 0:
+                        counter -= 1
+                left += 1
+                
+            right += 1
+
+           
+        if not containsAll: return ""
+        return s[min_left:min_left + minsublen]
+        
